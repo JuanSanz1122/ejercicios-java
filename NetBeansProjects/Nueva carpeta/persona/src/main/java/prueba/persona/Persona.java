1b0f0a7ab4c7f1caa7d76fa8c39524afb5733068
Juan Sanz <juansanzgmz@gmail.com>
2024-10-05 12:44:07 -0500

    primer commit

---
+++ NetBeansProjects/Nueva carpeta/persona/src/main/java/prueba/persona/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prueba.persona;
+
+/**
+ *
+ * @author juans
+ */
+public class Persona {
+
+    public static void main(String[] args) {
+        Datospersona persona1 = new Datospersona("Juan", 18, "Masculino");
+        persona1.mostrarInformacion();
+    }
+}
+
