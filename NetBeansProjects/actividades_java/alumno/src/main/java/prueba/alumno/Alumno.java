5eba9297ba9d47728026660af4c91d39ef13652f
Juan Sanz <juansanzgmz@gmail.com>
2024-10-05 12:23:59 -0500

    nuevo commit

---
+++ NetBeansProjects/actividades_java/alumno/src/main/java/prueba/alumno/Alumno.java
@@ -0,0 +1,12 @@
+
+
+package prueba.alumno;
+
+
+public class Alumno {
+
+    public static void main(String[] args) {
+        datos alumno1 = new datos (5, " maria ", " aristisabal ");
+        System.out.print("El id del alumno: " + alumno1.getId());
+    }
+}
