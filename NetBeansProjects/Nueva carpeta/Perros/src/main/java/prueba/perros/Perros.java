36e81c6ac9ef337c543b30413e9c66e9ea19f564
Juan Sanz <juansanzgmz@gmail.com>
2024-10-05 12:43:41 -0500

    primer commit

---
+++ NetBeansProjects/Nueva carpeta/Perros/src/main/java/prueba/perros/Perros.java
@@ -0,0 +1,12 @@
+
+
+package prueba.perros;
+
+
+public class Perros {
+
+    public static void main(String[] args) {
+        Datos perro1 = new Datos (1, " coco ", " frenchpudul ");
+        System.out.print("El id del perro es: " + perro1.getId());
+    }
+}
