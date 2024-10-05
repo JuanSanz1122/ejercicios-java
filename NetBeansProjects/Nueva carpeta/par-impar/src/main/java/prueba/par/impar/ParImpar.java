ee70458a373d54720db6b0632b055f6f2b7024ff
Juan Sanz <juansanzgmz@gmail.com>
2024-10-05 12:43:15 -0500

    primer commit

---
+++ NetBeansProjects/Nueva carpeta/par-impar/src/main/java/prueba/par/impar/ParImpar.java
@@ -0,0 +1,19 @@
+
+package prueba.par.impar;
+
+import java.util.Scanner;
+
+public class ParImpar {
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+    System.out.println(" Digite su numero ");
+    int num = scanner.nextInt();
+           if (num % 2 == 0) { 
+            System.out.print("el numero es par");}
+           else {
+                System.out.print("el numero no es par");}
+           }
+    }
+
+
